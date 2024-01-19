76901a21c2545a3326557cef840fe73dc46b928b
chylaa <chylaa45@op.pl>
2024-01-19 21:20:17 +0100

    Cached build files removed.

--- MaszynaPi/bin/Debug/scripts/JoystickHandler.py
+++
@@ -1,8 +0,0 @@
-import sys
-from sense_hat import SenseHat
-sense = SenseHat()
-while True:
-    for e in sense.stick.get_events():       
-        if e.action=="pressed":
-            print(str(e.direction))
-            sys.stdout.flush()
